5bd565429c151bd650d6d8fd2adc4e1289f772ef
rucko24 <rucko24@gmail.com>
2016-10-17 22:18:47 -0430

    Home

--- src/main/java/com/Core/vaadin/pageLayout/bodyLayout/contentLayout/ContentLayout.java
+++ src/main/java/com/Core/vaadin/pageLayout/bodyLayout/contentLayout/ContentLayout.java
@@ -1,11 +1,10 @@
 package com.Core.vaadin.pageLayout.bodyLayout.contentLayout;
 
-import com.Core.vaadin.pageLayout.bodyLayout.menuLayout.MenuLayout;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
@@ -13,25 +12,32 @@ import com.vaadin.ui.themes.ValoTheme;
 public class ContentLayout extends VerticalLayout {
 	
 	private Label label = new Label("<h1>Bienvenido<h1>", ContentMode.HTML);
-	private CssLayout csslayout = new CssLayout();
-	private HorizontalLayout top = new HorizontalLayout();
+	private VerticalLayout verticalLayout = new VerticalLayout();
 	private ThemeResource moderFoca = new ThemeResource("img/moderFOca.jpg");
 	private Label moderFoca2 = new Label();
 	
 	public ContentLayout() {
-	
-		addComponent(getLayout());
+		
+		Component area = getLayout();
+		
+		addComponent(area);
+		setComponentAlignment(area, Alignment.MIDDLE_CENTER);
 	}
 	
 	public Component getLayout() {
 		
+		label.setSizeUndefined();
 		label.addStyleName("labelMenu");
 		moderFoca2.setIcon(moderFoca);
-		csslayout.addComponents(label,moderFoca2);
-		csslayout.addStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+		moderFoca2.setSizeUndefined();
 		
-		top.addComponent(csslayout);
+		verticalLayout.setSizeFull();
+		verticalLayout.setMargin(true);
+		verticalLayout.addComponents(label, moderFoca2);
+		verticalLayout.setComponentAlignment(label, Alignment.BOTTOM_CENTER);
+		verticalLayout.setComponentAlignment(moderFoca2, Alignment.BOTTOM_CENTER);
+
 		
-		return top;
+		return verticalLayout;
 	}
 }
